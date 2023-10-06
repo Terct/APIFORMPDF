353166a90aa589ddb85a0e6ca8cf1f2489ed1a7a
Terct <servercentralterct@gmail.com>
2023-10-06 17:29:27 -0300

    first commit

--- app.js
+++ app.js
@@ -230,6 +230,33 @@ function generateRandomId(length) {
   return id;
 }
 
+
+app.post('/gerar-pdf', (req, res) => {
+  // Aqui você deve obter os dados do corpo da solicitação POST
+
+  const requestData = req.body; // Suponha que os dados sejam enviados como JSON no corpo da solicitação
+  // Agora você pode usar requestData para criar o PDF
+
+  // Cria um novo documento PDF
+  const doc = new PDFDocument();
+  doc.pipe(fs.createWriteStream('output.pdf')); // Salva o PDF em um arquivo chamado 'output.pdf'
+
+  // Cabeçalho do PDF
+  doc.fontSize(16).text('Exemplo de PDF gerado a partir de um array', 50, 50);
+
+  // Organize as perguntas em grupos de 5 e crie uma tabela para cada grupo
+  const groupSize = 5;
+  for (let i = 0; i < requestData.length; i += groupSize) {
+    const group = requestData.slice(i, i + groupSize);
+    criarTabela(doc, group);
+  }
+
+  // Finalize o PDF e envie-o como resposta
+  doc.end();
+  res.status(200).send('PDF gerado com sucesso!');
+});
+
+
 function criarTabela(doc, group) {
   // Configuração da tabela
   const tableX = 50;
